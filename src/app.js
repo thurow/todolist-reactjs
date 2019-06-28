9790ce14b59118279aff76fad8b5986c884379d9
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2019-06-28 13:05:19 -0300

    styling application

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Form from './components/Form'
-import TodosList from './components/TodosList';
-import Filter from './components/Filter';
-import SearchCep from './components/SearchCep';
+import TodosList from './components/TodosList'
+import Filter from './components/Filter'
+import SearchCep from './components/SearchCep'
+
+import 'milligram'
 
 const App = () => (
-  <div>
+  <div style={{ display: 'flex', justifyContent: 'space-around', padding: 30 }}>
     <div>
       <Form />
       <TodosList />
