ebffe20acf664df191d2872e5322800539778aaa
Alexandre Thurow <alexandre.fredericothurow@gmail.com>
2019-06-28 12:54:15 -0300

    initial structure for search cep

--- src/components/SearchCep/index.js
+++ src/components/SearchCep/index.js
@@ -3,7 +3,31 @@ import React from 'react'
 const SearchCep = () => {
     return (
         <div>
-            CEP
+            <form>
+                <input type='text' name='cep' />
+                <button type='submit'>Buscar Endereço</button>
+            </form>
+
+            <table>
+                <thead>
+                    <tr>
+                        <td>CEP</td>
+                        <td>Endereço</td>
+                        <td>Bairro</td>
+                        <td>Cidade</td>
+                        <td>Estado</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>89086-723</td>
+                        <td>Rua Rio de Janeiro</td>
+                        <td>Estados</td>
+                        <td>Indaial</td>
+                        <td>Santa Catarina</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     )
 }
